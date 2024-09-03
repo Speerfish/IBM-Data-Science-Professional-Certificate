9fbf369f7cb60250619b43b5b1a6479102503be9
Speerfish <sirarmin@gmail.com>
2024-09-02 21:39:20 -0600

    Add files via upload

--- Applied Data Science Capstone/Module 3/spacex_dash_app.py
+++ Applied Data Science Capstone/Module 3/spacex_dash_app.py
@@ -12,7 +12,7 @@ max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
 launch_sites = []
-launch_sites.append({'label': 'All Sites', 'value': 'All Sites'})
+launch_sites.append({'label': 'All Launch Sites', 'value': 'All Sites'})
 all_launch_sites = spacex_df['Launch Site'].unique().tolist()
 for launch_site in all_launch_sites:
     launch_sites.append({'label': launch_site, 'value': launch_site})
@@ -31,7 +31,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                     dcc.Dropdown(
                                         id = 'site-dropdown',
                                         options = launch_sites,
-                                        placeholder = 'Select a Launch Site here',
+                                        placeholder = 'Select a Launch Site.',
                                         searchable = True ,
                                         clearable = False,
                                         value = 'All Sites'
@@ -84,14 +84,14 @@ def update_piegraph(site_dropdown):
         fig = px.pie(
                 data,
                 names = 'Launch Site',
-                title = 'Total Success Launches by All Sites',
+                title = 'Successful Launches at All Sites',
             )
     else:
         data  = spacex_df.loc[spacex_df['Launch Site'] == site_dropdown]
         fig = px.pie(
                 data,
                 names = 'class',
-                title = 'Total Success Launches for Site &#8608; '+site_dropdown,
+                title = 'Total Successful Launches at Site &#8608; '+site_dropdown,
             )
     return fig
 
@@ -100,7 +100,7 @@ def update_piegraph(site_dropdown):
 @app.callback(
      Output(component_id = 'success-payload-scatter-chart', component_property = 'figure'),
      [Input(component_id = 'site-dropdown', component_property = 'value'), 
-     Input(component_id = "payload_slider", component_property = "value")]
+     Input(component_id = 'payload_slider', component_property = 'value')]
 )
 def update_scattergraph(site_dropdown,payload_slider):
     if (site_dropdown == 'All Sites' or site_dropdown == 'None'):
@@ -111,10 +111,10 @@ def update_scattergraph(site_dropdown,payload_slider):
                 data[inrange], 
                 x = "Payload Mass (kg)", 
                 y = "class",
-                title = 'Correlation Between Payload and Success for All Sites',
-                color="Booster Version Category",
-                size='Payload Mass (kg)',
-                hover_data=['Payload Mass (kg)']
+                title = 'Correlation Between Payload & Launch Success for All Launch Sites',
+                color='Booster Rocket Version Category',
+                size='Booster RocketPayload Mass (kg)',
+                hover_data=['Booster Rocket Payload Mass (kg)']
             )
     else:
         low, high = payload_slider
@@ -122,10 +122,10 @@ def update_scattergraph(site_dropdown,payload_slider):
         inrange = (data['Payload Mass (kg)'] > low) & (data['Payload Mass (kg)'] < high)
         fig = px.scatter(
                 data[inrange],
-                x = "Payload Mass (kg)",
+                x = "Booster Rocket Payload Mass (kg)",
                 y = "class",
-                title = 'Correlation Between Payload and Success for Site &#8608; '+site_dropdown,
-                color="Booster Version Category",
+                title = 'Correlation Between Payload and Launch Success for Site &#8608; '+site_dropdown,
+                color='Booster Rocket Version Category',
                 size='Payload Mass (kg)',
                 hover_data=['Payload Mass (kg)']
             )
